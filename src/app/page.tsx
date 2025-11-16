7793458d1e323108957b676faa69608e8db68358
someswargorai <goraisomeswar@gmail.com>
2025-11-16 16:10:58 +0530

    feat: change some line to test pipeline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
 
           <h1 className="text-5xl md:text-6xl font-bold leading-tight">
             World’s Most Powerful Recipessss in.
-            <br /> One App.
+            <br /> One App. Built by Someswar
           </h1>
 
           <p className="text-gray-400 text-lg">
